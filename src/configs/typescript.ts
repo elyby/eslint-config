a6667fb421dd453bf0070f8c9ee995a43c8f15ce
ErickSkrauch <erickskrauch@yandex.ru>
2019-12-16 13:59:31 +0300

    Upgrade configuration to be compatible with eslint 6 and latest changes in the eslint-typescript

--- src/configs/typescript.ts
+++ src/configs/typescript.ts
@@ -5,7 +5,7 @@ const config: Linter.Config = {
 
     // @ts-ignore
     plugins: [
-        '@typescript-eslint',
+        '@typescript-eslint/eslint-plugin',
     ],
 
     overrides: [
@@ -24,35 +24,39 @@ const config: Linter.Config = {
                 'react/prop-types': 'off',
 
                 '@typescript-eslint/adjacent-overload-signatures': 'error',
-                '@typescript-eslint/array-type': ['error', 'generic'],
+                '@typescript-eslint/array-type': ['error', {
+                    default: 'generic',
+                    readonly: 'generic',
+                }],
                 '@typescript-eslint/camelcase': 'error',
                 '@typescript-eslint/class-name-casing': 'error',
+                '@typescript-eslint/consistent-type-assertions': 'error',
+                '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
                 '@typescript-eslint/explicit-member-accessibility': 'error',
                 '@typescript-eslint/func-call-spacing': 'error',
                 '@typescript-eslint/indent': 'error',
                 '@typescript-eslint/interface-name-prefix': ['error', 'never'],
                 '@typescript-eslint/member-delimiter-style': 'error',
-                '@typescript-eslint/no-angle-bracket-type-assertion': 'error',
                 '@typescript-eslint/no-array-constructor': 'error',
                 '@typescript-eslint/no-empty-interface': 'error',
                 '@typescript-eslint/no-extra-parens': ['warn', 'all', {
                     nestedBinaryExpressions: false,
                     ignoreJSX: 'multi-line',
                 }],
-                '@typescript-eslint/no-inferrable-types': 'error',
+                '@typescript-eslint/no-inferrable-types': ['error', {
+                    ignoreParameters: true,
+                }],
                 '@typescript-eslint/no-misused-new': 'error',
                 '@typescript-eslint/no-namespace': 'error',
-                '@typescript-eslint/no-object-literal-type-assertion': 'error',
                 '@typescript-eslint/no-parameter-properties': 'error',
-                '@typescript-eslint/no-triple-slash-reference': 'error',
                 '@typescript-eslint/no-unused-vars': 'error',
                 '@typescript-eslint/no-use-before-define': 'error',
                 '@typescript-eslint/no-useless-constructor': 'warn',
                 '@typescript-eslint/no-var-requires': 'error',
                 '@typescript-eslint/prefer-for-of': 'warn',
-                '@typescript-eslint/prefer-interface': 'error',
                 '@typescript-eslint/prefer-namespace-keyword': 'error',
                 '@typescript-eslint/semi': 'error',
+                '@typescript-eslint/triple-slash-reference': 'error',
                 '@typescript-eslint/type-annotation-spacing': 'error',
             },
         },
